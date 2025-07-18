8b3ec70d48791f68704b83a2fc5e1608b1ec7a38
hoope123 <princeapi17@gmail.com>
2025-07-18 23:05:36 +0100

    Update index.js

--- gift/index.js
+++ gift/index.js
@@ -7,11 +7,11 @@ const config = require('../set'),
       { gmd, commands, events } = require('./princecmds'),
       { GiftedAntidelete } = require('./princemsgdel'),
       { eventlogger } = require('./gmdlogger'),
-      { saveMessage } = require('./gmddatabase'),
+      { saveMessage, loadMessage } = require('./gmddatabase'),
       { GiftedAnticall, GiftedFancy, Giftedttstalk, GiftedApkDl, giftedCdn, makeId, loadSession, convertStickerToImage, giftedTempmail, getBuffer, monospace, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, toAudio, toPTT, eBasef, dBasef, eBinary, dBinary, toVideo, ffmpeg, fetchJson,  getTempMail,
     getTempMailInbox,
     getTempMailMessage } = require('./princefunction');
 
 module.exports = { GiftedAnticall, GroupUpdate, GiftedFancy, eventlogger, saveMessage, loadSession, Giftedttstalk, giftedCdn, makeId, GiftedApkDl, giftedTempmail, giftedProcessImage, giftedHd2, getBuffer, monospace, getGroupAdmins, getRandom, h2k, isUrl, Json, config, runtime, sleep, toAudio, toPTT, eBasef, dBasef, eBinary, dBinary, toVideo, ffmpeg, fetchJson, emojis, doReact, giftedmd, downloadMediaMessage, gmd, GiftedAntidelete, convertStickerToImage, commands, getSudoNumbers, addSudo, removeSudo, events,  getTempMail,
     getTempMailInbox,
-    getTempMailMessage };
+    getTempMailMessage, loadMessage };
